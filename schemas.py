0bf3e27148ea24fe527d2018512f227b93c67d45
Andres Sanchez <afsanchez93@gmail.com>
2022-12-08 17:34:49 -0500

    add validator to skills field in user schema

--- schemas.py
+++ schemas.py
@@ -1,9 +1,9 @@
 from typing import List, Optional
 
-from pydantic import BaseModel, Json, EmailStr
+from pydantic import BaseModel, Json, EmailStr, root_validator
 import uuid
 
-from pydantic.fields import Dict
+from pydantic.fields import Dict, Field
 
 
 class UserBase(BaseModel):
@@ -11,7 +11,18 @@ class UserBase(BaseModel):
     last_name: str
     email: EmailStr
     years_previous_experience: int
-    skills: List[Dict[str, int]]
+    skills: List[Dict[str, int]] = Field(example=[{"python": 5}, {"sql": 3}])
+
+    @root_validator(pre=True)
+    def validate_skills(cls, values):
+        if not isinstance(values.get('skills'), list):
+            raise ValueError('skills must be a list')
+        for skill in values.get('skills'):
+            if not isinstance(skill, dict):
+                raise ValueError('skills must be a list of dicts')
+            if len(skill) != 1:
+                raise ValueError('skills must be a list of dicts with one key')
+        return values
 
     class Config:
         orm_mode = True
