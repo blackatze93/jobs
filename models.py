4ae2985f5819b6a1017398b9df6a31c7a1534b72
Andres Sanchez <afsanchez93@gmail.com>
2022-12-08 16:49:53 -0500

    add models in SQLAlchemy

---
+++ models.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, JSON
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import UUIDType, EmailType, URLType, CurrencyType, JSONType
+import uuid
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(UUIDType(binary=False), primary_key=True, index=True, default=uuid.uuid4)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(EmailType, unique=True, index=True)
+    years_previous_experience = Column(Integer)
+    skills = Column(JSONType)
+
+
+class Company(Base):
+    __tablename__ = "companies"
+    id = Column(UUIDType(binary=False), primary_key=True, index=True, default=uuid.uuid4)
+    name = Column(String)
+    email = Column(EmailType, unique=True, index=True)
+    website = Column(URLType)
+
+    vacancies = relationship("Vacancy", back_populates="company")
+
+
+class Vacancy(Base):
+    __tablename__ = "vacancies"
+    id = Column(UUIDType(binary=False), primary_key=True, index=True, default=uuid.uuid4)
+    position_name = Column(String)
+    salary = Column(Integer)
+    currency = Column(CurrencyType)
+    link = Column(URLType)
+    required_skills = Column(JSONType)
+
+    company_id = Column(UUIDType(binary=False), ForeignKey("companies.id"))
+    company = relationship("Company", back_populates="vacancies")
+
+
+
